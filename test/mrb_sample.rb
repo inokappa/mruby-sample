5e6f4afc8b31398a1f49d61850db45162536c307
inokappa <inokara@gmail.com>
2015-11-01 10:02:30 +0900

    test

--- test/mrb_sample.rb
+++ test/mrb_sample.rb
@@ -2,16 +2,7 @@
 ## Sample Test
 ##
 
-assert("Sample#hello") do
-  t = Sample.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Sample#bye") do
-  t = Sample.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Sample.hi") do
-  assert_equal("hi!!", Sample.hi)
+assert("DatadogClient#hello") do
+  t = DatadogClient.new("hello")
+  assert_equal("Say hello", t.hello)
 end
