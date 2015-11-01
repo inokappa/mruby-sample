d7ccfc0e817ec6fc953389fcb300ceb6235cffec
inokappa <inokara@gmail.com>
2015-11-01 13:52:09 +0900

    test

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,8 +1,8 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-httprequest'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-json'
   conf.gem '../mruby-sample'
-  conf.add_test_dependency 'mruby-httprequest'
-  conf.add_test_dependency 'mruby-json'
   conf.enable_test
 end
